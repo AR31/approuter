86634f620d99a981d117d84c4d350485fb154683
AR31 <arwa.khlifi31@gmail.com>
2020-11-23 00:35:50 +0100

    creat componennt

---
+++ src/movie/movie.js
@@ -0,0 +1,29 @@
+import React from "react";
+const listMovie = [
+  {
+    title: "title1",
+    description: "description1",
+    posterUrl: "http://film1.com",
+    rate: "1",
+  },
+  {
+    title: "title2",
+    description: "description2",
+    posterUrl: "http://film2.com",
+    rate: "2",
+  },
+  {
+    title: "title3",
+    description: "description3",
+    posterUrl: "http://film1.com",
+    rate: "3",
+  },
+  {
+    title: "title4",
+    description: "description4",
+    posterUrl: "http://film4.com",
+    rate: "4",
+  },
+];
+function movie() {}
+export default movie;
